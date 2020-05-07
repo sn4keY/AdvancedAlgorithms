a89ea95976a441f6d5962e36c8a880b1ffd36f4d
Norbert Neudert <norbertneudert@gmail.com>
2020-05-07 12:38:04 +0200

    Added Person class

---
+++ AdvancedAlgorithms/AdvancedAlgorithms/Problems/WorkAssignment/Person.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdvancedAlgorithms.Problems.WorkAssignment
+{
+    public class Person
+    {
+        public float Salary;
+        public float Quality;
+
+        public Person(float salary, float quality)
+        {
+            this.Salary = salary;
+            this.Quality = quality;
+        }
+    }
+}
