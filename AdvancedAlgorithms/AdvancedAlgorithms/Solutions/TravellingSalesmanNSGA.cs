4d50f02e16bcdf7af66b0c04d414287b16e314b2
Norbert Neudert <norbertneudert@gmail.com>
2020-05-07 14:59:16 +0200

    Added solution class

---
+++ AdvancedAlgorithms/AdvancedAlgorithms/Solutions/TravellingSalesmanNSGA.cs
@@ -0,0 +1,25 @@
+﻿using AdvancedAlgorithms.Algorithms;
+using AdvancedAlgorithms.Problems.TravellingSalesman;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdvancedAlgorithms.Solutions
+{
+    class TravellingSalesmanNSGA
+    {
+        private TravellingSalesmanProblem problem;
+        private NSGA solverAlgorithm;
+
+        public TravellingSalesmanNSGA(TravellingSalesmanProblem problem, NSGA algorithm)
+        {
+            this.problem = problem;
+            this.solverAlgorithm = algorithm;
+            this.problem.LoadTownsFromFile("inputTowns.txt");
+        }
+
+
+    }
+}
