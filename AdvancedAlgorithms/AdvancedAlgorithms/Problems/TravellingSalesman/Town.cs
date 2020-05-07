0dd0e51dda52731ffd70d615c5076acff8f56a8d
Norbert Neudert <norbertneudert@gmail.com>
2020-05-07 12:05:11 +0200

    Added Town class

---
+++ AdvancedAlgorithms/AdvancedAlgorithms/Problems/TravellingSalesman/Town.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdvancedAlgorithms.Problems.TravellingSalesman
+{
+    public class Town
+    {
+        public float X;
+        public float Y;
+
+        public Town(float x, float y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+    }
+}
